f083b93670aacde83d11860c7046165baad4a2e0
patrick chan <kingsmanwufan@gmail.com>
2025-05-22 15:20:03 +0800

    add server

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['1cb6c10b-fab9-4ef8-a00f-5aa87e2e2bb7-00-1gvyld6f0kfgd.sisko.replit.dev']
+  }
 })
